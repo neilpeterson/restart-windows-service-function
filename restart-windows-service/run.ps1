567183d0f5f98fe4860ee9edfbd2719b8d06715e
Neil Peterson <neilpeterson@outlook.com>
2020-10-07 20:43:09 -0700

    Update run.ps1

--- restart-windows-service/run.ps1
+++ restart-windows-service/run.ps1
@@ -1,7 +1,7 @@
 using namespace System.Net
 
 # Input bindings are passed in via param block.
-# param($Request, $TriggerMetadata)
+param($Request, $TriggerMetadata)
 
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
