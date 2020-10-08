d376674766f3e2d4c93e8646804782861862b657
Neil Peterson <neilpeterson@outlook.com>
2020-10-07 21:53:03 -0700

    Update run.ps1

--- restart-windows-service/run.ps1
+++ restart-windows-service/run.ps1
@@ -6,20 +6,28 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
-# # Interact with query parameters or the body of the request.
-# $name = $Request.Query.Name
-# if (-not $name) {
-#     $name = $Request.Body.Name
-# }
+Write-Output "========================"
+print-output $Request
+Write-Output "========================"
 
-# $body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+Write-Output "========================"
+print-output $TriggerMetadata
+Write-Output "========================"
 
-# if ($name) {
-#     $body = "Hello, $name. This HTTP triggered function executed successfully."
-# }
+# Interact with query parameters or the body of the request.
+$name = $Request.Query.Name
+if (-not $name) {
+    $name = $Request.Body.Name
+}
 
-# # Associate values to output bindings by calling 'Push-OutputBinding'.
-# Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-#     StatusCode = [HttpStatusCode]::OK
-#     Body = $body
-# })
+$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+
+if ($name) {
+    $body = "Hello, $name. This HTTP triggered function executed successfully."
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $body
+})
